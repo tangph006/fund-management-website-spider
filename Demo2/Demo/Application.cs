dd8266ffd5fdceecb0a3454f3237acda6d152d0b
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-06 07:05:55 +0000

    Code migration from Demo2 to my WPF demo

---
+++ Demo2/Demo/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Demo
+{
+    public partial class App : Application
+    {
+        // user settings
+        public SettingT settingT;
+        // user input
+        public InputT inputT;
+        // output of parse/crawl result
+        public OutputT outputT;
+        // the hard working man
+        public ParserT parserT;
+    }
+}
