b0689dc4d6c8693aa7b128f46af4e038419be868
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-06 07:05:55 +0000

    Code migration from Demo2 to my WPF demo

---
+++ trunk/Demo2/Demo/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Demo.App app = new Demo.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            else
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd";
+                p.Start();
+            }
+        }
+    }
+}
