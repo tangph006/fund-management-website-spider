a6a88661046b9364301844c281d2cb60d351cadf
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-07 10:21:10 +0000

    json sample decorator sample

---
+++ Demo2/DecratorSample1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w = new Rifle();
+            w.ShowInfo();
+            Enhance enhancedWeapon = new Enhance(w);
+            enhancedWeapon.EnhanceAmmo();
+            enhancedWeapon.ShowInfo();
+            Wear wornWeapon = new Wear(w);
+            wornWeapon.WearByRate(0.8);
+            wornWeapon.ShowInfo();
+        }
+    }
+
+    abstract class Weapon
+    {
+        private double ammo;
+
+        public double Ammo
+        {
+            get { return ammo; }
+            set { ammo = value; }
+        }
+
+        private double attack;
+
+        public double Attack
+        {
+            get { return attack; }
+            set { attack = value; }
+        }
+
+        private double speed;
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public abstract void ShowInfo();
+    }
+
+    class Rifle : Weapon
+    {
+        public Rifle()
+        {
+            this.Ammo = 100;
+            this.Attack = 10;
+            this.Speed = 5;
+            this.Name = "Rifle";
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine(string.Format("ammo\t{0}", Ammo));
+            Console.WriteLine(string.Format("attack\t{0}", Attack));
+            Console.WriteLine(string.Format("speed\t{0}", Speed));
+            Console.WriteLine(string.Format("name\t{0}", Name));
+        }
+    }
+
+    abstract class Decorator : Weapon
+    {
+        protected Weapon w;
+
+        public Decorator(Weapon w)
+        {
+            this.w = w;
+        }
+
+        public override void ShowInfo()
+        {
+            w.ShowInfo();
+        }
+    }
+
+    class Enhance : Decorator
+    {
+        public Enhance(Weapon w) : base(w) { }
+
+        public void EnhanceAmmo()
+        {
+            w.Ammo += 20;
+            Console.WriteLine(">>>>>>>>>>>>Enhanced");
+        }
+    }
+
+    class Wear : Decorator
+    {
+        public Wear(Weapon w) : base(w) { }
+        public void WearByRate(double rate)
+        {
+            w.Speed = w.Speed * rate;
+            w.Attack = w.Attack * rate;
+            Console.WriteLine(">>>>>>>>>>>>Worn");
+        }
+    }
+}
---
+++ Demo2/JsonSample2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace JsonSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonStr = MyWriteOjbect();
+            Console.ReadLine();
+            var config = new Config()
+            {
+                encoding_ = "UTF-8",
+                plugins_ = new string[] { "python", "C++", "C#" },
+                indent_ = new Indent() { length_ = 4, useSpace_ = false, test_ = 10000 }
+            };
+            string strTest = config.ToString();
+        }
+
+        static string MyWriteOjbect()
+        {
+            var config = new Config()
+            {
+                encoding_ = "UTF-8",
+                plugins_ = new string[] { "python", "C++", "C#" },
+                indent_ = new Indent() { length_ = 4, useSpace_ = false, test_ = 10000 }
+            };
+            var serializer = new DataContractJsonSerializer(typeof(Config));
+            var stream = new MemoryStream();
+            serializer.WriteObject(stream, config);
+
+            byte[] dataBytes = new byte[stream.Length];
+
+            stream.Position = 0;
+
+            stream.Read(dataBytes, 0, (int)stream.Length);
+            string dataString = Encoding.UTF8.GetString(dataBytes);
+            Console.WriteLine("JSON string is:");
+            Console.WriteLine(dataString);
+            return dataString;
+        }
+
+        static void MyReadObject(string dataString)
+        {
+            var mStream = new MemoryStream(Encoding.Default.GetBytes(dataString));
+            var serializer = new DataContractJsonSerializer(typeof(Config));
+            Config readConfig = (Config)serializer.ReadObject(mStream);
+
+            Console.WriteLine("encoding_: {0}", readConfig.encoding_);
+            Console.WriteLine("indent.length_: {0}", readConfig.indent_.length_);
+            foreach (string plugin in readConfig.plugins_)
+            {
+                Console.WriteLine("plugins: {0}", plugin);
+            }
+            
+            Console.WriteLine("indent.useSpace_: {0}", readConfig.indent_.useSpace_);
+            Console.WriteLine("indent.test_: {0}", readConfig.indent_.test_);
+        }
+    }
+
+    [DataContract]
+    class Config
+    {
+        private string encoding;
+        [DataMember(Order = 0)]
+        public string encoding_
+        {
+            get { return encoding; }
+            set { encoding = value; }
+        }
+
+        private string[] plugins;
+        [DataMember(Order = 1)]
+        public string[] plugins_
+        {
+            get { return plugins; }
+            set { plugins = value; }
+        }
+
+        private Indent indent;
+        [DataMember(Order = 2)]
+        public Indent indent_
+        {
+            get { return indent; }
+            set { indent = value; }
+        }
+    }
+
+    [DataContract]
+    class Indent
+    {
+        private int length;
+        [DataMember(Order = 0)]
+        public int length_
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        private bool useSpace;
+        [DataMember(Order = 1)]
+        public bool useSpace_
+        {
+            get { return useSpace; }
+            set { useSpace = value; } 
+        }
+
+        private int test;
+        [DataMember(Order = 1)]
+        public int test_
+        {
+            get { return test; }
+            set { test = value; }
+        }
+    }
+}
