d6eac4f8124d4c30fb41397d1acd07e4316abd81
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-19 05:59:33 +0000

    WCF demo

---
+++ trunk/WCFDemo/Hosting/Program.cs
@@ -0,0 +1,32 @@
+﻿ using System;
+ using System.ServiceModel;
+using System.ServiceModel.Description;
+ using Artech.WcfServices.Contracts;
+ using Artech.WcfServices.Services;
+namespace Artech.WcfServices.Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
+            {
+                host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:9999/calculatorservice");
+                if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                {
+                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                    behavior.HttpGetEnabled = true;
+                    behavior.HttpGetUrl = new Uri("http://127.0.0.1:9999/calculatorservice/metadata");
+                    host.Description.Behaviors.Add(behavior);
+                }
+                host.Opened += delegate
+                {
+                    Console.WriteLine("Host program is running, press any key to quit...");
+                };
+
+                host.Open();
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ trunk/WCFDemo/Services/CalculatorService.cs
@@ -0,0 +1,26 @@
+﻿using Artech.WcfServices.Contracts;
+namespace Artech.WcfServices.Services
+{
+    public class CalculatorService : ICalculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
