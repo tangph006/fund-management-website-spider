f5e72b0843c9b8b0c8f4c59b994be034924f7244
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-19 05:59:33 +0000

    WCF demo

---
+++ WCFDemo/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Client.ServiceReference1;
+
+namespace Artech.WcfServices.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            {
+                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                Console.Read();
+            }
+        }
+    }
+}
+
