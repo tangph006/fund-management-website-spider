0adbc7e08f7c7c22b83c14081fabe1bd630c8452
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-20 04:11:02 +0000

    mark

--- trunk/WCFDemo/Client/Program.cs
+++ trunk/WCFDemo/Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Client.ServiceReference1;
+using System.ServiceModel;
+using Artech.WcfServices.Contracts;
 
 namespace Artech.WcfServices.Client
 {
@@ -7,12 +8,17 @@ namespace Artech.WcfServices.Client
     {
         static void Main(string[] args)
         {
-            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>("CalculatorService"))
             {
-                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
-                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
-                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
-                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                ICalculator proxy = channelFactory.CreateChannel();
+                using (proxy as IDisposable)
+                {
+                    Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                    Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                    Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                    Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                
+                }
                 Console.Read();
             }
         }
