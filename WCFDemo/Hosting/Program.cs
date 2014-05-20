fbe819304e6322be05097fdb429cc188de9f8a7e
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-05-20 04:11:02 +0000

    mark

--- WCFDemo/Hosting/Program.cs
+++ WCFDemo/Hosting/Program.cs
@@ -11,14 +11,14 @@ namespace Artech.WcfServices.Hosting
         {
             using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
             {
-                host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:9999/calculatorservice");
-                if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
-                {
-                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
-                    behavior.HttpGetEnabled = true;
-                    behavior.HttpGetUrl = new Uri("http://127.0.0.1:9999/calculatorservice/metadata");
-                    host.Description.Behaviors.Add(behavior);
-                }
+//                 host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:9999/calculatorservice");
+//                 if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+//                 {
+//                     ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+//                     behavior.HttpGetEnabled = true;
+//                     behavior.HttpGetUrl = new Uri("http://127.0.0.1:9999/calculatorservice/metadata");
+//                     host.Description.Behaviors.Add(behavior);
+//                 }
                 host.Opened += delegate
                 {
                     Console.WriteLine("Host program is running, press any key to quit...");
