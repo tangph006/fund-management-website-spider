fb0348a1522b188e18e616a57ea29c9fd34cbb22
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-12-14 16:00:44 +0000

    my sort

--- trunk/TestUnicodeIO/TestUnicodeIO/TabSubView4.h
+++ trunk/TestUnicodeIO/TestUnicodeIO/TabSubView4.h
@@ -50,20 +50,21 @@ protected:
     // sort [m_vec[first_], m_vec[end]]
     void _Sort_my(size_t first_, size_t last_)
     {
-        int count_;
-        for(; (count_= last_ - first_)>32; )
+        int count_ = last_-first_+1;
+        while(count_ > 32)
         {
             IteratorRange mid_ = _Unguarded_partition_my(first_, last_);
             if(mid_.m_first - first_ < last_ - mid_.m_last)
             {
-                _Sort_my(first_, mid_.m_first);
-                first_ = mid_.m_last;
+                _Sort_my(first_, mid_.m_first-1);
+                first_ = mid_.m_last+1;
             }
             else
             {
-                _Sort_my(mid_.m_last, last_);
-                last_ = mid_.m_first;
+                _Sort_my(mid_.m_last+1, last_);
+                last_ = mid_.m_first-1;
             }
+            count_ = last_-first_+1;
         }
 
         if(1<count_ && count_<=32)
@@ -96,7 +97,7 @@ protected:
     }
     void _Median_my(size_t first_, size_t mid_, size_t last_)
     {
-        int length_ = last_ - first_;
+        int length_ = last_ - first_ +1;
         if(length_ > 40)
         {
             int nStep = (length_+1)/8;
@@ -128,9 +129,9 @@ protected:
 
         for( ; ; )
         {
-            for(; right_begin_ < last_; ++right_begin_)
+            for(; right_begin_ < last_; right_begin_++)
             {
-                if(m_vec[mid_begin_] < m_vec[right_begin_])
+                if(m_vec[right_begin_] > m_vec[mid_begin_])
                 {
 
                 }
@@ -140,25 +141,25 @@ protected:
                 }
                 else
                 {
-                    iter_swap_my(mid_end_, right_begin_);
+                    iter_swap_my(right_begin_, mid_end_);
                     ++mid_end_;
                 }
             }
 
-            for(; first_<left_end_; --left_end_)
+            for(; left_end_>first_; left_end_--)
             {
                 if(m_vec[left_end_-1] < m_vec[mid_begin_])
                 {
 
                 }
-                else if(m_vec[mid_begin_] < m_vec[left_end_-1])
+                else if(m_vec[left_end_-1] > m_vec[mid_begin_])
                 {
                     break;
                 }
                 else
                 {
                     --mid_begin_;
-                    iter_swap_my(mid_begin_, left_end_-1);
+                    iter_swap_my(left_end_-1, mid_begin_);
                 }
             }
 
@@ -166,7 +167,7 @@ protected:
             {
                 IteratorRange pair_;
                 pair_.m_first = mid_begin_;
-                pair_.m_last = mid_end_;
+                pair_.m_last = mid_end_-1;
                 return pair_;
             }
 
@@ -209,36 +210,49 @@ protected:
         }
         m_vec[first_+hole_] = val_;
     }
-    void _Adjust_heap_my(size_t first_, int hole_, int bottom_, int val_)
+    void _Adjust_heap_my(size_t first_, int hhh121, int bottom_, int val_)
     {
-        int top_ = hole_;
-        int idx_ = 2*hole_ + 2;
+        int top_ = hhh121;
+        int idx_ = 2*hhh121 + 2;
         for(; idx_<bottom_; idx_ = idx_*2 +2)
         {
             if(m_vec[first_+idx_] < m_vec[first_+idx_-1])
             {
                 --idx_;
             }
-            m_vec[first_+hole_] = m_vec[first_+idx_];
-            hole_ = idx_;
+            m_vec[first_+hhh121] = m_vec[first_+idx_];
+            hhh121 = idx_;
         }
 
         if(idx_ == bottom_)
         {
-            m_vec[first_+hole_] = m_vec[first_+bottom_-1];
-            hole_ = bottom_ - 1;
+            m_vec[first_+hhh121] = m_vec[first_+bottom_-1];
+            hhh121 = bottom_ - 1;
         }
 
-        _Push_heap_my(first_, hole_, top_, val_);
+        for(int idx_ = (hhh121-1)/2; top_<hhh121 && m_vec[first_+idx_]<val_; idx_ = (hhh121-1)/2)
+        {
+            m_vec[first_+hhh121] = m_vec[first_+idx_];
+            hhh121 = idx_;
+        }
+        m_vec[first_+hhh121] = val_;
     }
     void _Make_heap_my(size_t first_, size_t last_)
     {
-        int bottom_ = last_ - first_;
-        for(int hole_ = bottom_/2; hole_>0; )
+        int count_ = last_-first_+1;
+        for(int cur_=count_/2; cur_>0; cur_--)
         {
-            --hole_;
-            int val_ = m_vec[first_ + hole_];
-            _Adjust_heap_my(first_, 0, last_-first_, val_);
+            int parent = first_+cur_-1;
+            int leftchild = first_+cur_*2-1;
+            int rightchild = first_+cur_*2;
+            if(m_vec[parent] > m_vec[leftchild])
+            {
+                iter_swap_my(parent, leftchild);
+            }
+            if(m_vec[parent] > m_vec[rightchild])
+            {
+                iter_swap_my(parent, rightchild);
+            }
         }
     }
     void make_heap_my(size_t first_, size_t last_)
