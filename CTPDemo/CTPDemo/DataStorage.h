a44a81b35cb1860689db95f371a84d72fc59344a
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-12-23 11:21:12 +0000

    file map

---
+++ CTPDemo/CTPDemo/DataStorage.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include "..\6.3.0_20140811_traderapi_win32\ThostFtdcUserApiStruct.h"
+
+class DataStorage
+{
+public:
+    DataStorage();
+    ~DataStorage();
+public:
+    void AddDepthMarketData(const CThostFtdcDepthMarketDataField* pDepthMarketData);
+
+protected:
+    // 大连商品交易所
+    std::vector<CThostFtdcDepthMarketDataField> m_vA;
+    std::vector<CThostFtdcDepthMarketDataField> m_vM;
+    std::vector<CThostFtdcDepthMarketDataField> m_vC;
+    std::vector<CThostFtdcDepthMarketDataField> m_vY;
+    std::vector<CThostFtdcDepthMarketDataField> m_vP;
+    std::vector<CThostFtdcDepthMarketDataField> m_vL;
+    std::vector<CThostFtdcDepthMarketDataField> m_vPVC;
+    // 上海期货交易所
+    std::vector<CThostFtdcDepthMarketDataField> m_vRU;
+    std::vector<CThostFtdcDepthMarketDataField> m_vCU;
+    std::vector<CThostFtdcDepthMarketDataField> m_vAL;
+    std::vector<CThostFtdcDepthMarketDataField> m_vZN;
+    std::vector<CThostFtdcDepthMarketDataField> m_vAU;
+    std::vector<CThostFtdcDepthMarketDataField> m_vFU;
+    std::vector<CThostFtdcDepthMarketDataField> m_vRB;
+    std::vector<CThostFtdcDepthMarketDataField> m_vWR;
+    // 郑州商品交易所
+    std::vector<CThostFtdcDepthMarketDataField> m_vWT;
+    std::vector<CThostFtdcDepthMarketDataField> m_vWS;
+    std::vector<CThostFtdcDepthMarketDataField> m_vSR;
+    std::vector<CThostFtdcDepthMarketDataField> m_vPTA;
+    std::vector<CThostFtdcDepthMarketDataField> m_vCF;
+    std::vector<CThostFtdcDepthMarketDataField> m_vRO;
+    std::vector<CThostFtdcDepthMarketDataField> m_vER;
+    // 中国金融交易所
+    std::vector<CThostFtdcDepthMarketDataField> m_vIF;
+};
+
