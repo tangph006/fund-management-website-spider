a3aec18b5eb5a1746200404dfccbf32d44ced856
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2014-12-24 11:31:43 +0000

    file mappingfile mapping version 3

---
+++ TestUnicodeIO/CreatingViewWithFile/aaaaaaaa.h
@@ -0,0 +1,11 @@
+#pragma once
+class aaaaaaaa
+{
+public:
+    int m_a;
+    int m_b;
+public:
+    aaaaaaaa(void);
+    ~aaaaaaaa(void);
+};
+
--- TestUnicodeIO/CreatingViewWithFile/main.cpp
+++ TestUnicodeIO/CreatingViewWithFile/main.cpp
@@ -1,164 +1,246 @@
 //http://bbs.csdn.net/topics/80299016
 #include <windows.h>
-#include <stdio.h>
 #include <tchar.h>
 #include "resource.h"
+#include "aaaaaaaa.h"
 #include <assert.h>
+#include <typeinfo>
 
 #define BUFFSIZE 1024
-#define FILE_MAP_START 0
+#define FILE_MAP_START 64
 TCHAR * lpcTheFile = TEXT("fmtest.txt");
+const static short initItemCount = 2048;
 
-class MyFileMapManager
+struct structBase
+{
+    int m_a;
+};
+
+struct structDerive : public structBase
+{
+    int m_b;
+};
+
+template<class T> class MyFileMapManager
 {
 public:
-    MyFileMapManager(){}
-    virtual ~MyFileMapManager(){}
-
-    void MapFile(int& iErr, TCHAR* pPath, DWORD dwFileMapStart)
+    MyFileMapManager():m_nDataCount(0),m_pData(0),m_hFile(0),m_hMap(0)
     {
-        HANDLE hFile = CreateFile(lpcTheFile, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-        if(hFile == INVALID_HANDLE_VALUE)
-        {
-            iErr = ID_INVALID_HANDLE_VALUE;
-            return;
-        }
-
-        SYSTEM_INFO SysInfo;
-        GetSystemInfo(&SysInfo);
-        DWORD dwSysGran = SysInfo.dwAllocationGranularity;
-        DWORD dwFileMapStart1 = 0/*dwFileMapStart*/;
-        _tprintf (TEXT("The file map view starts at %ld bytes into the file.\n"), dwFileMapStart1);
-        DWORD dwMapViewSize = (FILE_MAP_START % dwSysGran) + BUFFSIZE;
-        _tprintf (TEXT("The file map view is %ld bytes large.\n"), dwMapViewSize);
-        DWORD dwFileMapSize = FILE_MAP_START + BUFFSIZE;
-        _tprintf (TEXT("The file mapping object is %ld bytes large.\n"), dwFileMapSize);
-        int iViewDelta = FILE_MAP_START - dwFileMapStart1;
-        _tprintf (TEXT("The data is %d bytes into the view.\n"), iViewDelta);
-        DWORD dBytesWritten;
-        for (int i=0; i<(int)dwSysGran; i++)
-        {
-            WriteFile (hFile, &i, sizeof (i), &dBytesWritten, NULL);
-        }
-
-        DWORD dwFileSize = GetFileSize(hFile,  NULL);
-        _tprintf(TEXT("hFile size: %10d\n"), dwFileSize);
-
-        HANDLE hMapFile = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, dwFileMapSize, NULL);
-        if (hMapFile == NULL)
-        {
-            _tprintf(TEXT("hMapFile is NULL: last error: %d\n"), GetLastError());
-            return;
-        }
-
-        LPVOID lpMapAddress = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, dwFileMapStart1, dwMapViewSize);
-        if (lpMapAddress == NULL)
-        {
-            _tprintf(TEXT("lpMapAddress is NULL: last error: %d\n"), GetLastError());
-            return;
-        }
-
-        char* p = (char*)lpMapAddress;
-        for(int i=0; i<100; i++)
-        {
-            *p = 'c';
-            p++;
-        }
-        int iRet = FlushViewOfFile(lpMapAddress, 100);
-        char * pData = (char *) lpMapAddress + iViewDelta;
-        int iData = *(int *)pData;
-        _tprintf (TEXT("The value at the pointer is %d,\nwhich %s one quarter of the desired file offset.\n"),
-            iData, iData*4 == FILE_MAP_START ? TEXT("is") : TEXT("is not"));
-        BOOL bRet = UnmapViewOfFile(lpMapAddress);
-        bRet = CloseHandle(hMapFile);
-        if(!bRet)
-        {
-            _tprintf(TEXT("Error %ld occurred closing the mapping object!\n"), GetLastError());
-        }
-
-        bRet = CloseHandle(hFile);
-        if(!bRet)
-        {
-            _tprintf(TEXT("Error %ld occurred closing the file!\n"), GetLastError());
-        }
-        return;
-
-
+        memset(m_strStructName, 0, 64);
+        const type_info& typeinfo = typeid(T);
+        const char* className = typeinfo.name();
+        strcpy(&m_strStructName[0], className);
     }
-    int GetDataCount() const { return m_nDataCount; }
-    void Increase(int iDelta=1) { m_nDataCount += iDelta; }
+    virtual ~MyFileMapManager()
+    {
+        if(m_pMapViewBegin)
+        {
+            UnmapViewOfFile(m_pMapViewBegin);
+            m_pMapViewBegin = NULL;
+        }
+        if(m_hMap)
+            CloseHandle(m_hMap);
+        if(m_hFile)
+            CloseHandle(m_hFile);
+    }
+
+    void MapToFile(int& iErr, TCHAR* pPath)
+    {
+        if(pPath == NULL)
+        {
+            iErr = ID_ERROR_NULL_POINTER;
+            return;
+        }
+
+        CreateFileIfNotExist(iErr, pPath);
+        if(iErr != ID_SUCCESS)
+            return;
+
+        m_nDataCount = GetDataCountFromFile(iErr, pPath);
+        if(iErr != ID_SUCCESS)
+            return;
+        m_n2 = m_nDataCount + 1024;
+
+        m_hFile = CreateFile(pPath, GENERIC_READ | GENERIC_WRITE, 0, 
+            NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (m_hFile == INVALID_HANDLE_VALUE)
+        {
+            iErr = ID_ERROR_OPEN_FILE_FAILED;
+            return;
+        }
+
+        m_hMap = CreateFileMapping(m_hFile, NULL, PAGE_READWRITE, 0, 68+sizeof(T)*m_n2, NULL);
+        if (m_hMap == NULL)
+        {
+            iErr = ID_ERROR_CREATE_FILE_MAPPING_FAILED;
+            CloseHandle(m_hFile);
+            return;
+        }
+
+        m_pMapViewBegin = MapViewOfFile(m_hMap, FILE_MAP_ALL_ACCESS, 0, 0, 68+sizeof(T)*m_n2);
+        if (m_pMapViewBegin == NULL)
+        {
+            int iErr = ID_ERROR_MAP_VIEW_OF_FILE_FAILED;
+            CloseHandle(m_hMap);
+            CloseHandle(m_hFile);
+            return;
+        }
+
+        int* pTemp = (int*)m_pMapViewBegin;
+        m_nDataCount = *pTemp;
+        pTemp++;
+        if(strcmp(m_strStructName, (char*)pTemp) != 0)
+        {
+            int iErr = ID_ERROR_NOT_CORRECT_DATATYPE;
+            return;
+        }
+        m_pData = ((T*)pTemp) + 16;
+        iErr = ID_SUCCESS;
+        return;
+    }
+
+
+    void AddItem(int& iErr, const T* pItem)
+    {
+        assert(pItem);
+        {
+            int sizeOfT = sizeof(T);
+            memcpy(&m_pData[m_nDataCount], pItem, sizeOfT);
+        }
+        int iWin32Err = GetLastError();
+        m_nDataCount++;
+        memcpy(m_pMapViewBegin, &m_nDataCount, 4);
+        FlushViewOfFile(m_pMapViewBegin, 4);
+        FlushViewOfFile((LPVOID)(m_pData+m_nDataCount), sizeof(T));
+        if(m_nDataCount == m_n2-2)
+        {
+            UnmapViewOfFile(m_pMapViewBegin);
+
+            m_n2 += 1024;
+            CloseHandle(m_hMap);
+            m_hMap = CreateFileMapping(m_hFile, NULL, PAGE_READWRITE, 0, 68+sizeof(T)*m_n2, NULL);
+            if (m_hMap == NULL)
+            {
+                iErr = ID_ERROR_CREATE_FILE_MAPPING_FAILED;
+                CloseHandle(m_hFile);
+                return;
+            }
+
+            m_pMapViewBegin = MapViewOfFile(m_hMap, FILE_MAP_ALL_ACCESS, 0, 0, 68+sizeof(T)*m_n2);
+            if (m_pMapViewBegin == NULL)
+            {
+                int iErr = ID_ERROR_MAP_VIEW_OF_FILE_FAILED;
+                CloseHandle(m_hMap);
+                CloseHandle(m_hFile);
+                return;
+            }
+        }
+    }
 
 protected:
+    void CreateFileIfNotExist(int &iErr, TCHAR* pPath)
+    {
+        HANDLE hFile = CreateFile(pPath, GENERIC_READ | GENERIC_WRITE, 0, 
+            NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hFile == INVALID_HANDLE_VALUE)
+        {
+            int iWin32Err = GetLastError();
+            if(iWin32Err == 2)
+            {
+                hFile = CreateFile(pPath, GENERIC_READ | GENERIC_WRITE, 0, 
+                    NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+                if (hFile == INVALID_HANDLE_VALUE)
+                {
+                    iErr = ID_ERROR_OPEN_FILE_FAILED;
+                    return;
+                }
+                DWORD tempDWord;
+                WriteFile(hFile, &m_nDataCount, 4, &tempDWord, NULL);
+                WriteFile(hFile, m_strStructName, 64, &tempDWord, NULL);
+                CloseHandle(hFile);
+                iErr = ID_SUCCESS;
+                return;
+            }
+            else
+            {
+                iErr = ID_ERROR_OPEN_FILE_FAILED;
+                return;
+            }
+        }
+        CloseHandle(hFile);
+        iErr = ID_SUCCESS;
+    }
+
+    int GetDataCountFromFile(int &iErr, TCHAR* pPath)
+    {
+        HANDLE hFile = CreateFile(pPath, GENERIC_READ | GENERIC_WRITE, 0, 
+            NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hFile == INVALID_HANDLE_VALUE)
+        {
+            iErr = ID_ERROR_OPEN_FILE_FAILED;
+            return -1;
+        }
+
+        LARGE_INTEGER fileSize;
+        ::GetFileSizeEx(hFile, &fileSize);
+
+        HANDLE hMap = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, 68, NULL);
+        if (hMap == NULL)
+        {
+            iErr = ID_ERROR_CREATE_FILE_MAPPING_FAILED;
+            CloseHandle(hFile);
+            return -1;
+        }
+
+        LPVOID pMapViewBegin = MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 68);
+        if (pMapViewBegin == NULL)
+        {
+            int iErr = ID_ERROR_MAP_VIEW_OF_FILE_FAILED;
+            CloseHandle(hMap);
+            CloseHandle(hFile);
+            return -1;
+        }
+
+        int* pTemp = (int*)pMapViewBegin;
+        int nDataCount = *pTemp;
+        pTemp++;
+        if(strcmp(m_strStructName, (char*)pTemp) != 0)
+        {
+            int iErr = ID_ERROR_NOT_CORRECT_DATATYPE;
+            return -1;
+        }
+
+        UnmapViewOfFile(pMapViewBegin);
+        CloseHandle(hMap);
+        CloseHandle(hFile);
+        iErr = ID_SUCCESS;
+        return nDataCount;
+    }
+
+protected:
+    LPVOID m_pMapViewBegin;
+    T* m_pData;
+private:
     int m_nDataCount;
-    void* m_pData;
+    char m_strStructName[64];
+    HANDLE m_hFile;
+    HANDLE m_hMap;
+    int m_n2;
 };
 
 int main()
 {
-    HANDLE hFile = CreateFile(lpcTheFile, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
+    structBase ss;
+    MyFileMapManager<aaaaaaaa> manager;
+    int iErr=0;
+    TCHAR* aaa= _T("1.txt");
+    manager.MapToFile(iErr, aaa);
+    for(int i=0; i<1000000; i++)
     {
-        _tprintf(TEXT("hFile is NULL\n"));
-        _tprintf(TEXT("Target file is %s\n"),
-            lpcTheFile);
-        return 4;
-    }
-
-    SYSTEM_INFO SysInfo;
-    GetSystemInfo(&SysInfo);
-    DWORD dwSysGran = SysInfo.dwAllocationGranularity;
-    DWORD dwFileMapStart = FILE_MAP_START;
-    _tprintf (TEXT("The file map view starts at %ld bytes into the file.\n"), dwFileMapStart);
-    DWORD dwMapViewSize = (FILE_MAP_START % dwSysGran) + BUFFSIZE;
-    _tprintf (TEXT("The file map view is %ld bytes large.\n"), dwMapViewSize);
-    DWORD dwFileMapSize = FILE_MAP_START + BUFFSIZE;
-    _tprintf (TEXT("The file mapping object is %ld bytes large.\n"), dwFileMapSize);
-    int iViewDelta = FILE_MAP_START - dwFileMapStart;
-    _tprintf (TEXT("The data is %d bytes into the view.\n"), iViewDelta);
-    DWORD dBytesWritten;
-    for (int i=0; i<(int)dwSysGran; i++)
-    {
-        WriteFile (hFile, &i, sizeof (i), &dBytesWritten, NULL);
-    }
-
-    _tprintf(TEXT("hFile size: %d\n"), GetFileSize(hFile,  NULL));
-
-    HANDLE hMapFile = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, dwFileMapSize, NULL);
-    if (hMapFile == NULL)
-    {
-        _tprintf(TEXT("hMapFile is NULL: last error: %d\n"), GetLastError());
-        return (2);
-    }
-
-    LPVOID lpMapAddress = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, dwFileMapStart, dwMapViewSize);
-    if (lpMapAddress == NULL)
-    {
-        _tprintf(TEXT("lpMapAddress is NULL: last error: %d\n"), GetLastError());
-        return 3;
-    }
-
-    char* p = (char*)lpMapAddress;
-    for(int i=0; i<100; i++)
-    {
-        *p = 'c';
-        p++;
-    }
-    int iRet = FlushViewOfFile(lpMapAddress, 100);
-    char * pData = (char *) lpMapAddress + iViewDelta;
-    int iData = *(int *)pData;
-    _tprintf (TEXT("The value at the pointer is %d,\nwhich %s one quarter of the desired file offset.\n"),
-        iData, iData*4 == FILE_MAP_START ? TEXT("is") : TEXT("is not"));
-    BOOL bRet = UnmapViewOfFile(lpMapAddress);
-    bRet = CloseHandle(hMapFile);
-    if(!bRet)
-    {
-        _tprintf(TEXT("Error %ld occurred closing the mapping object!\n"), GetLastError());
-    }
-
-    bRet = CloseHandle(hFile);
-    if(!bRet)
-    {
-        _tprintf(TEXT("Error %ld occurred closing the file!\n"), GetLastError());
+        aaaaaaaa newA;
+        newA.m_a = 10000;
+        newA.m_b = 10000;
+        manager.AddItem(iErr, &newA);
     }
     return 0;
 }
