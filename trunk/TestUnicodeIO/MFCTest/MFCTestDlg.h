1f299a1f92b895bb2674a27f3edaef5f5f94387b
tangph006 <tangph006@467f1194-8387-25e8-f23a-cd3fd7002e70>
2015-01-06 10:30:51 +0000

    pioneer interview

--- trunk/TestUnicodeIO/MFCTest/MFCTestDlg.h
+++ trunk/TestUnicodeIO/MFCTest/MFCTestDlg.h
@@ -1,31 +1,22 @@
-
-// MFCTestDlg.h : header file
-//
-
 #pragma once
 
-
-// CMFCTestDlg dialog
 class CMFCTestDlg : public CDialogEx
 {
-// Construction
 public:
-	CMFCTestDlg(CWnd* pParent = NULL);	// standard constructor
+    CMFCTestDlg(CWnd* pParent = NULL);
+    enum { IDD = IDD_MFCTEST_DIALOG };
 
-// Dialog Data
-	enum { IDD = IDD_MFCTEST_DIALOG };
-
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
-
-
-// Implementation
 protected:
-	HICON m_hIcon;
+    virtual void DoDataExchange(CDataExchange* pDX);
 
-	// Generated message map functions
-	virtual BOOL OnInitDialog();
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
-	DECLARE_MESSAGE_MAP()
+protected:
+    HICON m_hIcon;
+    virtual BOOL OnInitDialog();
+    afx_msg void OnPaint();
+    afx_msg HCURSOR OnQueryDragIcon();
+    DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnBnClickedBegin();
+    int m_nTotal;
+    afx_msg void OnBnClickedBtnBegin2();
 };
--- trunk/TestUnicodeIO/MFCTest/Resource.h
+++ trunk/TestUnicodeIO/MFCTest/Resource.h
@@ -2,17 +2,19 @@
 // Microsoft Visual C++ generated include file.
 // Used by MFCTest.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_MFCTEST_DIALOG				102
+#define IDD_MFCTEST_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_BEGIN                   1000
+#define IDC_EDIT_TOTAL                  1001
+#define IDC_BTN_BEGIN2                  1002
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
